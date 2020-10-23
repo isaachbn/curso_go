49d8da993fab561febd037f4aefdd2f8e06dd9cd
Isaac Nunes <isaac.nunes@digivox.com.br>
2020-10-23 07:26:41 -0300

    Finalizando estudos

--- http/dinamico/dinamico.go
+++ http/dinamico/dinamico.go
@@ -1,1 +1,27 @@
-package dinamico
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+func horaCerta(w http.ResponseWriter, r *http.Request)  {
+	/**
+	  02 - representa dia
+	  01 - representa mês
+	  2006 - reprrsenta ano
+	  03 - representa hora
+	  04 - representa minuto
+	  05 - representa segundo
+	**/
+	s := time.Now().Format("02/01/2006 03:04:05")
+	fmt.Fprintf(w, "<h1>HORA CERT: %s</h1>", s)
+}
+
+func main() {
+	http.HandleFunc("/horaCerta", horaCerta)
+	log.Println("Executando")
+	log.Fatal(http.ListenAndServe(":3000", nil))
+}
